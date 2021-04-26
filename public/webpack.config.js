88a7f78d101030e4e7cdbfe1e629a1997bb2ebb3
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-04-25 20:31:25 -0700

    webpack created

---
+++ public/webpack.config.js
@@ -0,0 +1,40 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const config = {
+    entry: {
+        app: './assets/js/index.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins:[
+        new WebpackPwaManifest({
+            fingerprints: false,
+            name: 'Newsy app',
+            short_name: 'Newsy',
+            description: 'An application that allows you to view different news articles and save your favorites.',
+            background_color: '#01579b',
+            theme_color: '#ffffff',
+            'theme-color': '#ffffff',
+            start_url: '/',
+        })
+    ],
+}
+
+module.exports = config;
